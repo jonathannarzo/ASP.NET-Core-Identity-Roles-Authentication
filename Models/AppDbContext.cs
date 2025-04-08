ec35d31d9a001de1e5959f90ecf42426e87ba31d
jonathan.narzo@gmail.com <jonathan.narzo@gmail.com>
2025-04-08 20:56:51 +0800

    re

--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -21,24 +21,23 @@ public class AppDbContext : IdentityDbContext<ApiUser>
     {
         base.OnModelCreating(builder);
 
-        // builder.ApplyConfiguration(new RoleConfiguration());
+        builder.ApplyConfiguration(new RoleConfiguration());
 
-        // test
-        // builder.Entity<ApiUser>(b =>
-        // {
-        //     b.HasMany(e => e.UserRoles)
-        //     .WithOne(e => e.User)
-        //     .HasForeignKey(ur => ur.UserId)
-        //     .IsRequired();
-        // });
+        builder.Entity<ApiUser>(b =>
+        {
+            b.HasMany(e => e.UserRoles)
+            .WithOne(e => e.User)
+            .HasForeignKey(ur => ur.UserId)
+            .IsRequired();
+        });
 
-        // builder.Entity<Roles>(b =>
-        // {
-        //     b.HasMany(e => e.UserRoles)
-        //     .WithOne(e => e.Role)
-        //     .HasForeignKey(ur => ur.RoleId)
-        //     .IsRequired();
-        // });
+        builder.Entity<Roles>(b =>
+        {
+            b.HasMany(e => e.UserRoles)
+            .WithOne(e => e.Role)
+            .HasForeignKey(ur => ur.RoleId)
+            .IsRequired();
+        });
     }
 
     public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
